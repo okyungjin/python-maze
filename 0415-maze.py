57a47b2b025d18e3d8a8454199560959689e7217
okyungjin <okyungjin@gmail.com>
2022-04-19 15:00:35 +0900

    WIP

--- 0415-maze.py
+++ 0415-maze.py
@@ -3,9 +3,9 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 f = open(os.path.join(__location__, 'maze1.txt'), 'r')
 
-def read_maze(n, m, f):
+def read_maze(f):
     maze = []
-    for i in range(cols):
+    for _ in range(rows):
         row = list(f.readline().rstrip())
         maze.append(row)
     return maze
@@ -14,23 +14,19 @@ def read_maze(n, m, f):
 def print_maze(maze):
     for i in range(rows):
         for j in range(cols):
-            if maze[i][j] == 'V': # VISITED
-                print('O', end = '')
-            elif maze[i][j] == 'B': # BACKTRACKED
-                print(' ', end = '')
-            else:
-                print(maze[i][j], end = '')
+            try:
+                cur = maze[i][j]
+                if cur== 'V': # VISITED
+                    print('o', end = '')
+                elif cur == 'B': # BACKTRACKED
+                    print(' ', end = '')
+                else:
+                    print(cur, end = '')
+
+            except IndexError:
+                print(rows, cols, i, j)
         print()
 
-
-n, m = map(int, f.readline().split())
-rows = 2 * n + 1
-cols = 2 * m + 1
-
-maze = read_maze(n, m, f)
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
 def dfs(cur_x, cur_y, stack):
     maze[cur_x][cur_y] = 'V';
@@ -59,7 +55,17 @@ def dfs(cur_x, cur_y, stack):
             print('No path exist')
             return
         pos = stack.pop()
-        print('pop', pos)
+        print('pop ', pos)
         dfs(pos[0], pos[1], stack)
+
+n, m = map(int, f.readline().split())
+
+cols = 2 * n + 1
+rows = 2 * m + 1
+
+maze = read_maze(f)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 dfs(1, 1, [])
--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Stack:
     def __init__(self):
         self.num = []
@@ -39,164 +41,142 @@ class Stack:
 
 data = []
 
-import os
-import pprint as pp
-import copy
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-f = open(os.path.join(__location__, 'maze1.txt'), 'r')
+with open('maze1.txt') as f:
+    size = f.readline()[:-1]
+    while True:
+        line = f.readline()
+        if not line: break
+        if '\n' in line:
+            if '\n' != line:
+                data.append(line[:-1])
+        else:
+            data.append(line)
 
-#with open('maze1.txt') as f:
-size = f.readline()[:-1]
-while True:
-    line = f.readline()
-    if not line: break
-    if '\n' in line:
-        if '\n' != line:
-            data.append(line[:-1])
-    else:
-        data.append(line)
-    
 # 불러온 파일의 내용을 우선 data 변수에 담는다.
 width=len(data[0])
 height=len(data)
-print(width, height)
 
+#미로 생성
 def make_maze(maze, width, height):
     newmaze = []
     for i in range(height):
         list = []
         for j in range(width):
-            if maze[i][j] == " ":
-                item_mod = '0' # 움직일 수 있음
+            if data[i][j] == " ":
+                item_mod = '0'
             else:
-                item_mod = '1' # 벽
+                item_mod = '1'
             list.append(item_mod)
         newmaze.append(list)
 
+    newmaze[-2][-2] = "x"
     return newmaze
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+#이동가능 여부
 def isValidPos(x, y):
     if x < 0 or y < 0 or x >= width or y >= height:
         return False
-    # else:
-    #     return maps[y][x] == '0' or maps[y][x] == 'x'
+    else:
+        return map[y][x] == '0' or map[y][x] == 'x'
 
-# global stack
+#미로 길 탐색 1번
+def DFS(s):
+    stack = Stack()
+    stack.push((1, 1))
+    print("push( 1 , 1 )")
+    while not stack.isEmpty():
+        here = stack.peek()
+        (x, y) = here
+        if (map[y][x] == 'x'):
+            map[y][x] = '.'
+            #1번 길은 무조건 출력
+            print("경로 1")
+            for i in range(height):
+                for j in range(width):
+                    if map[i][j] == '.':
+                        print('o', end='')
+                    else:
+                        print(data[i][j], end='')
+                print()
+            #1번 길을 저장한다. map3에 1번 길 저장 , map은 처음 make_maze의 결과를 저장했던 map2에 저장
+            #.의 위치가 중요하고 어느 쪽을 갔다 돌아왔는지는 출력시의 결과적 루트와는 관련이 없으므로 0으로 변경해줌
+            for i in range(height):
+                for j in range(width):
+                    map3[i][j] = map[i][j]
+                    if map3[i][j] == '2':
+                        map3[i][j] = '0'
+                    map[i][j] = map2[i][j]
+            return map3
 
-def DFS(x, y):
-    global maps
-    
-    if x < 1 or y < 1 or x >= width-1 or y >= height-1:
-        return False
+        else:
+            map[y][x] = '.'
+            if isValidPos(x, y - 1):
+                stack.push((x, y - 1))
+                print("push(",x,',',y-1,')')
+            elif isValidPos(x, y + 1):
+                stack.push((x, y + 1))
+                print("push(",x,',',y+1,')')
+            elif isValidPos(x - 1, y):
+                stack.push((x - 1, y))
+                print("push(",x-1,',',y,')')
+            elif isValidPos(x + 1, y):
+                stack.push((x + 1, y))
+                print("push(",x+1,',',y,')')
+            #그 길이 아니면 2번으로 바꾸고 pop한다
+            else:
+                map[y][x] = '2'
+                here = stack.pop()
+                print("pop ", here)
+    return False
 
-    # print('push', x, y)
-    # if x % 2 == 0 and y % 2 == 0:
-        # print('pop', x % 2, y % 2)
+#미로 길 탐색 2번
+def DFS2(s):
+    stack = Stack()
+    stack.push((1, 1))
+    while not stack.isEmpty():
+        here = stack.peek()
+        (x, y) = here
+        if (map[y][x] == 'x'):
+            map[y][x] = '.'
+            #DFS 함수와 같이 지나왔는데 아니었던 점을 0으로 변경
+            for i in range(height):
+                for j in range(width):
+                    if map[i][j] == '2':
+                        map[i][j] = '0'
+            return map
+        #탐색방향을 바꾸었다. 상하좌우에서 상좌우하 로 바꾸어 아래로 가는걸 가장 마지막에 탐색하도록 했다.
+        else:
+            map[y][x] = '.'
+            if isValidPos(x, y - 1):
+                stack.push((x, y - 1))
+            elif isValidPos(x - 1, y):
+                stack.push((x - 1, y))
+            elif isValidPos(x + 1, y):
+                stack.push((x + 1, y))
+            elif isValidPos(x, y + 1):
+                stack.push((x, y + 1))
+            else:
+                map[y][x] = '2'
+                here = stack.pop()
+    return False
 
+map = make_maze(data, width, height)
+map2 = make_maze(data, width, height)
+map3 = make_maze(data, width, height)
 
-    if x == width - 2 and y == height - 2:
-        print('=====================YE=====================')
-        return True
-    
-    # stack.push((x, y))
-    # print('push', x, y)
+DFS(map)
+DFS2(map)
 
-    if not maps[x][y] == '0':
-        return False
-
-    maps[x][y] = 'V'
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        
-        # if nx >= 1 and ny >= 1 and nx <= width-1 and ny <= height-1 and maps[nx][ny] == '0':    
-        DFS(nx, ny)
-        
-        # print('pop', nx, ny)
-    maps[x][y] = '0'
-    return True
-
-        
-
-
-        # if nx % 2 == 0 and ny % 2 == 0:
-        #     print('push', x % 2, y % 2)
-    # maps[x][y] = '0'    
-
-    # maps[x][y] = '0'
-
-    # while not stack.isEmpty():
-    #     here = stack.pop()
-    #     print("pop ",here)
-    #     (x, y) = here
-    #     print(maps[x][y])
-        
-            # print(stack.dump())
-            # return True
-        # else:
-        #     maps[y][x] = '.'
-            
-        #     if isValidPos(x, y - 1):
-                
-        #         stack.push((x, y - 1))
-        #         print("push",x,y-1)
-        #     if isValidPos(x, y + 1):
-        #         stack.push((x, y + 1))
-        #         print("push",x,y+1)
-        #     if isValidPos(x - 1, y):
-        #         stack.push((x - 1, y))
-        #         print("push",x-1,y)
-        #     if isValidPos(x + 1, y):
-        #         stack.push((x + 1, y))
-        #         print("push",x+1,y)
-            # print(stack.dump())
-
-
-    # return False
-
-for d in data:
-    print(d)
-
-newmaze = make_maze(data, width, height)
-
-for maze in newmaze:
-    print(maze)
-
-maps = newmaze
-
-# stack = Stack()
-print('길 탐색 시작')
-
-# result = DFS(1, 1)
-
-# if result:
-#     print(' --> 미로탐색 성공')
-# else:
-#     print(' --> 미로탐색 실패')
-
-
-# . 지나옴
-count = 0
-graph = {
-	1: [2,3,4],
-	2:[5],
-	3:[5],
-	4:[],
-	5:[6,7],
-	6:[],
-	7:[3],
-}
-def stack_dfs(start_vertex):
-	visited = []
-	stack = [start_vertex]
-	while stack:
-		vertex = stack.pop()
-		if vertex not in visited:
-				visited.append(vertex)
-				for item in graph[vertex]:
-					stack.append(item)
-	return visited
-
-print(stack_dfs(1))
+#2번째 길이 있을 경우
+if not np.array_equal(map, map3):
+    print("경로 2")
+    for i in range(height):
+        for j in range(width):
+            if map[i][j] == '.':
+                print('o', end='')
+            else:
+                print(data[i][j], end='')
+        print()
+    print("길은 총 2개")
+else:
+    print("길은 총 1개")
